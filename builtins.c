fb63047b36d8537673ecf02ad9695179512bda84
giodueck <giodueck@gmail.com>
2020-11-19 13:26:54 -0300

    Added cd, help, and exit

--- builtins.c
+++ builtins.c
@@ -16,3 +16,35 @@ int num_builtins()
     return sizeof(builtin_str) / sizeof(char*);
 }
 
+int dish_cd(char **args)
+{
+    if (args[1] == NULL)
+    {
+        fprintf(stderr, "dish: se espera un argumento para \"cd\"\n");
+    } else if (chdir(args[1]) != 0)
+    {
+        perror("dish");
+    }
+
+    return 1;
+}
+
+int dish_help(char **args)
+{
+    printf("DISH: Giovanni Dueck's Interactive Shell\n");
+    printf("      Ingresa un comando y sus argumentos y presiona enter.\n");
+    printf("      Los comandos integrados son:\n");
+
+    for (int i = 0; i < num_builtins(); i++)
+    {
+        printf("        %s\n", builtin_str[i]);
+    }
+    
+    printf ("\n");
+    return 1;
+}
+
+int dish_exit(char **args)
+{
+    return 0;
+}

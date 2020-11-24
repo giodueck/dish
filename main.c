83109b3e15a2fbe0c4effa93a96e174df93d8393
giodueck <giodueck@gmail.com>
2020-11-24 11:13:17 -0300

    Added history logging

--- main.c
+++ main.c
@@ -19,39 +19,6 @@
 #include "defines.h"
 #include "dish.h"
 
-// Loop central del shell
-void dish_loop()
-{
-    char *line;
-    char **args;
-    char *current_dir = malloc (sizeof(char) * CURRDIR_BUFSIZE);
-    int status = 0;
-
-    if (!current_dir) // error de malloc
-    {
-        fprintf(stderr, "dish: allocation error\n");
-        exit(FAILURE);
-    }
-
-    do
-    {
-        // Se imprime el directotio actual y se lee el comando a ejecutarse
-        current_dir = getcwd(current_dir, CURRDIR_BUFSIZE);
-        printf("[%s] > ", current_dir);
-        line = dish_read_line();
-        args = dish_split_line(line);
-        status = dish_execute(args);
-
-        // Como a line y args se asignan bloques de memoria dinamica hay que liberarlos 
-        free(line);
-        free(args);
-
-        // Mientras que status != 0, el shell sigue leyendo comandos
-    } while (status);
-    
-    free(current_dir);
-}
-
 // Revisa si los archivos log necesarios existen, y si no, los crea
 void check_logs()
 {
@@ -90,6 +57,54 @@ void check_logs()
         fprintf(log, "LOG creado %d-%02d-%02d %02d:%02d:%02d\n\n", tms.tm_year + 1900, tms.tm_mon + 1, tms.tm_mday, tms.tm_hour, tms.tm_min, tms.tm_sec);
         fclose(log);
     }
+}
+
+// Agrega una entrada al log de historial
+void log_add(char *line)
+{
+    time_t t = time(NULL);
+    struct tm tms = *localtime(&t);
+    FILE *log = fopen("/var/log/dish/dish.log", "w");
+    
+    fprintf(log, "[%d-%02d-%02d %02d:%02d:%02d] %s\n", tms.tm_year + 1900, tms.tm_mon + 1, tms.tm_mday, tms.tm_hour, tms.tm_min, tms.tm_sec, line);
+    
+    fclose(log);
+}
+
+// Loop central del shell
+void dish_loop()
+{
+    char *line;
+    char **args;
+    char *current_dir = malloc (sizeof(char) * CURRDIR_BUFSIZE);
+    int status = 0;
+
+    if (!current_dir) // error de malloc
+    {
+        fprintf(stderr, "dish: allocation error\n");
+        exit(FAILURE);
+    }
+
+    do
+    {
+        // Se imprime el directotio actual y se lee el comando a ejecutarse
+        current_dir = getcwd(current_dir, CURRDIR_BUFSIZE);
+        printf("[%s] > ", current_dir);
+        line = dish_read_line();
+        args = dish_split_line(line);
+        status = dish_execute(args);
+
+        // Historial
+        log_add(line);
+
+        // Como a line y args se asignan bloques de memoria dinamica hay que liberarlos 
+        free(line);
+        free(args);
+
+        // Mientras que status != 0, el shell sigue leyendo comandos
+    } while (status);
+    
+    free(current_dir);
 }
 
 int main (int argc, char **argv)

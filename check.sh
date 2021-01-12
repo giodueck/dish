0e16799c19b02a2fe0413f8b48555489c9964eed
giodueck <giodueck@gmail.com>
2021-01-12 11:36:51 -0300

    Added /var/log/shell creation to check.sh

--- check.sh
+++ check.sh
@@ -6,3 +6,7 @@ if [ ! -f "/usr/lib/$name.so" ]; then
 	echo "$name no se encuentra instalado en /usr/lib"
 	exit
 fi
+if [ ! -d "/var/log/shell" ]; then
+	mkdir -v /var/log/shell
+	chmod +w /var/log/shell
+fi
